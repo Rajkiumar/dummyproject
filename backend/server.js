f224dab9908a42d027cc19cc066b70eee1707245
Rajkiumar <sumathiselvam0007@gmail.com>
2024-04-05 13:19:37 +0530

    Updated server.js and .env file for mongodb

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,25 @@ const app = express();
 const port = process.env.PUBLIC_PORT || 3000;
 const dotenv = require('dotenv');
 dotenv.config();
+const mongoose = require("mongoose")
+const mongobduri = process.env.MONGODB_URI
 
 // define the /env route with the response in JSON
 
 app.get('/ping', (req,res)=> {
   res.json({message:"pong"});
+})
+
+// conecting mongodb
+mongoose.connect(mongobduri)
+
+// Defining a route to check the mongoose connecting status
+app.get('/mongodbConnection0', (req,res)=> {
+  if (mongoose.connection.readyState === 1){
+    res.status(200).send(`MongoDB connected successfully`);
+  } else {
+    res.status(400).send('Error connecting to mongodb')
+  }
 })
 
 if (require.main === module) {
